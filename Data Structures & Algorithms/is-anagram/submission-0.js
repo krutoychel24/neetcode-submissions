6b15308340499be071f1f7c0a70c49e4f8938ed1
Maksym <84977480+krutoychel24@users.noreply.github.com>
2026-05-08 22:38:46 +0200

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length){
+            return false
+        }
+
+        let countS = {}
+        let countT = {}
+
+        for (let i = 0; i < s.length; i++) {
+            countS[s[i]] = 1 + (countS[s[i]] || 0);
+            countT[t[i]] = 1 + (countT[t[i]] || 0);
+        }
+
+        for (let c in countS) {
+            if (countS[c] !== countT[c])
+            {
+                return false
+            }
+        }
+        return true;
+    }
+}
